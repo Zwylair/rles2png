383339df27c1c3682dc713f189647e263fe335dc
Zwylair <82669735+Zwylair@users.noreply.github.com>
2024-01-21 22:56:23 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using Pfim;
+using s4pi.ImageResource;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Invalid arguments lenght. Using: exe <input_file> <output_file | print>");
+            Environment.Exit(1);
+        }
+
+        FileStream opened_rles = null;
+
+        try
+        {
+            opened_rles = new FileStream(args[0], FileMode.Open);
+        } catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Environment.Exit(1);
+        }
+
+        var rleResource = new RLEResource(1, opened_rles);
+        var image = Pfimage.FromStream(rleResource.ToDDS());
+
+        var dataHandle = Marshal.AllocHGlobal(image.DataLen);
+        Marshal.Copy(image.Data, 0, dataHandle, image.DataLen);
+
+        using (Bitmap bitmap = new Bitmap(image.Width, image.Height, image.Stride, System.Drawing.Imaging.PixelFormat.Format32bppArgb, dataHandle))
+        {
+            if (args[1] == "print")
+            {
+                using (MemoryStream mstream = new MemoryStream())
+                {
+                    bitmap.Save(mstream, System.Drawing.Imaging.ImageFormat.Png);
+
+                    byte[] byteArray = mstream.ToArray();
+                    string base64String = Convert.ToBase64String(byteArray);
+
+                    Console.WriteLine(base64String);
+                }
+            } else
+            {
+                try
+                {
+                    using (FileStream fstream = new FileStream(args[1], FileMode.Create))
+                    {
+                        bitmap.Save(fstream, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(1);
+                }
+                
+            }
+        }
+        Marshal.FreeHGlobal(dataHandle);
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# rles2png
+
+<p align="center">
+    <img src="https://img.shields.io/badge/LICENSE-MIT-green?style=for-the-badge">
+</p>
+
+rles2png is console program that converts dxt4 rles/rle2 image into more popular png format
+
+#### Features:
+* Save png into file OR print png in base64 to get past saving
+
+# License
+
+This project is under the [MIT license](./LICENSE).
